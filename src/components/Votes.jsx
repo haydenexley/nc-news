67b819d668b7963289bd38dbf845f360f7d3b5a9
haydenexley <haydenexley@outlook.com>
2023-09-05 16:39:13 +0100

    complete ticket 8 and add error handling for voting

--- src/components/Votes.jsx
+++ src/components/Votes.jsx
@@ -3,18 +3,23 @@ import { patchVotes } from "./utils";
 
 const Votes = ({ votes, article_id }) => {
   const [currentVotes, setCurrentVotes] = useState(votes);
+  const [error, setError] = useState(null)
 
   const userVote = (inc) => {
-    setCurrentVotes(currentVotes + inc)
+    setCurrentVotes((localVotes) => localVotes + inc)
 
-    patchVotes(article_id, inc).then(({ data: { article: { votes } } }) => {
-      setCurrentVotes(votes);
-    });
+    patchVotes(article_id, inc).catch(() => {
+      setError("Woopsie! There was an error voting! Please try again later...")
+    })
   };
+  if (error){
+    return <p>{error}</p>
+  }
 
   return (
     <div className="votes">
       <p>Votes: {currentVotes} 🗳</p>
+      {error && <p>{error}</p>}
       <button onClick={() => userVote(1)}>+</button>
       <button onClick={() => userVote(-1)}>-</button>
     </div>
